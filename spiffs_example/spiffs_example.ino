72723dd0d4d07984c24c8670df0c7fd0afb107b0
unknown <maepu234649@yahoo.co.jp>
2019-04-23 09:34:42 +0900

    Debug complete

--- spiffs_example/spiffs_example.ino
+++ spiffs_example/spiffs_example.ino
@@ -1,31 +1,29 @@
 #include "esp32_spiffs.h"
 
-// コンストラクタ
 ESP32_SPIFFS spiffs;
 
 char file_buf[16384];
 
 void setup() { 
   Serial.begin(115200);
+  memset(file_buf,0,sizeof(file_buf));
   spiffs.begin();
   
+
+  // ファイルをリスト表示
   spiffs.listDir("/", 0);
-//  spiffs.writeFile("/hello.txt", "Hello ");
-//  spiffs.appendFile("/hello.txt", "World!\r\n");
+  // ファイルを作成して書き込み
+  spiffs.writeFile("/hello.txt", "Hello ");
+  spiffs.appendFile("/hello.txt", "World!\n");
   spiffs.readFile("/demo.bmp", file_buf);
-  int i;
-  for(i=0;i<8192;i++){
-    Serial.printf("%x",file_buf[i]);
-  }
-  
-//  spiffs.renameFile("/hello.txt", "/foo.txt");
-//  spiffs.readFile("/foo.txt");
-//  spiffs.deleteFile("/foo.txt");
+  spiffs.renameFile("/hello.txt", "/foo.txt");
+  spiffs.readFile("/foo.txt",file_buf);
+  Serial.printf("%s\n",file_buf);
+  spiffs.deleteFile("/foo.txt");
 //  spiffs.testFileIO("/test.txt");
 //  spiffs.deleteFile("/test.txt");
   Serial.println( "Test complete" );  
 }
 
 void loop() {
-
 }
