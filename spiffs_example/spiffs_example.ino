bf10138c7522e07d821d4d2292b41a04a0ff598e
unknown <maepu234649@yahoo.co.jp>
2019-04-24 09:39:05 +0900

    first release

--- spiffs_example/spiffs_example.ino
+++ spiffs_example/spiffs_example.ino
@@ -9,16 +9,18 @@ void setup() {
   memset(file_buf,0,sizeof(file_buf));
   spiffs.begin();
   
-
+  // ファイルを作成して文字列を書き込む
+  spiffs.writeFile("/hello.txt", "Hello ");
+  // 指定したファイルに文字列を追記
+  spiffs.appendFile("/hello.txt", "World!\n");
   // ファイルをリスト表示
   spiffs.listDir("/", 0);
-  // ファイルを作成して書き込み
-  spiffs.writeFile("/hello.txt", "Hello ");
-  spiffs.appendFile("/hello.txt", "World!\n");
-  spiffs.readFile("/demo.bmp", file_buf);
+  // 指定したファイル名を変更
   spiffs.renameFile("/hello.txt", "/foo.txt");
+  // 指定したファイルを読み込む
   spiffs.readFile("/foo.txt",file_buf);
   Serial.printf("%s\n",file_buf);
+  // 指定したファイルを削除
   spiffs.deleteFile("/foo.txt");
 //  spiffs.testFileIO("/test.txt");
 //  spiffs.deleteFile("/test.txt");
